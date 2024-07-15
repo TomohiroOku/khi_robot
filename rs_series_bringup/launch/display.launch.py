abbbf348aef76d93035afb7fb3542b953dac28fe
Tomohiro Oku <tomohiro.oku@gmail.com>
2024-07-15 16:18:40 +0000

    add mock control

---
+++ rs_series_bringup/launch/display.launch.py
@@ -0,0 +1,47 @@
+import os
+
+import xacro
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    pkg_robot_description = get_package_share_directory("rs_series_bringup")
+
+    robot_description = xacro.process_file(
+        os.path.join(pkg_robot_description, "urdf", "rs007l.urdf.xacro")
+    ).toxml()
+    rviz_config = os.path.join(
+        get_package_share_directory("rs_series_description"), "config", "rs_series.rviz"
+    )
+    ros2_controllers = os.path.join(
+        pkg_robot_description, "config", "rs007l_controllers.yaml"
+    )
+
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description}],
+        output="screen",
+    )
+
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[ros2_controllers],
+        output="screen",
+    )
+
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=["-d", rviz_config],
+        output="screen",
+    )
+
+    return LaunchDescription([robot_state_publisher, controller_manager])
