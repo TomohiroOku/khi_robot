d5a8edb285862ad9dd5261c1a4f4369b3072a355
Tomohiro Oku <tomohiro.oku@gmail.com>
2024-07-16 10:33:02 +0000

    load controller

--- rs_series_bringup/launch/display.launch.py
+++ rs_series_bringup/launch/display.launch.py
@@ -3,7 +3,12 @@ import os
 import xacro
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    RegisterEventHandler,
+)
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
@@ -37,6 +42,20 @@ def generate_launch_description():
         output="screen",
     )
 
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+        output="screen",
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_trajectory_controller"],
+        output="screen",
+    )
+
     rviz = Node(
         package="rviz2",
         executable="rviz2",
@@ -44,4 +63,18 @@ def generate_launch_description():
         output="screen",
     )
 
-    return LaunchDescription([robot_state_publisher, controller_manager])
+    delay = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[robot_controller_spawner, rviz],
+        )
+    )
+
+    return LaunchDescription(
+        [
+            robot_state_publisher,
+            controller_manager,
+            joint_state_broadcaster_spawner,
+            delay,
+        ]
+    )
