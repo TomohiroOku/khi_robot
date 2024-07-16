8b23e2e00af74e504b518bdb2aa7b96f47c6becc
Tomohiro Oku <tomohiro.oku@gmail.com>
2024-07-16 03:16:12 +0000

    create package

---
+++ robot_hardware/include/robot_hardware/robot_hardware_interface.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "robot_hardware/visibility_control.h"
+
+namespace robot_hardware
+{
+
+  class RobotHardwareInterface
+  {
+  public:
+    RobotHardwareInterface();
+
+    virtual ~RobotHardwareInterface();
+  };
+
+} // namespace robot_hardware
---
+++ robot_hardware/src/robot_hardware_interface.cpp
@@ -0,0 +1,14 @@
+#include "robot_hardware/robot_hardware_interface.hpp"
+
+namespace robot_hardware
+{
+
+RobotHardwareInterface::RobotHardwareInterface()
+{
+}
+
+RobotHardwareInterface::~RobotHardwareInterface()
+{
+}
+
+}  // namespace robot_hardware
