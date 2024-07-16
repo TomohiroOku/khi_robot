5ba137014435701e1bd316d6b50262e933cddce4
Tomohiro Oku <tomohiro.oku@gmail.com>
2024-07-16 07:20:39 +0000

    implement simple hardware interfaces

--- robot_hardware/include/robot_hardware/robot_system.hpp
+++ robot_hardware/include/robot_hardware/robot_system.hpp
@@ -2,8 +2,8 @@
 
 #include <string>
 #include <vector>
+#include <unordered_map>
 
-#include "robot_hardware/visibility_control.h"
 #include "hardware_interface/system_interface.hpp"
 #include "rclcpp/macros.hpp"
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
@@ -16,24 +16,31 @@ namespace robot_hardware
   public:
     RCLCPP_SHARED_PTR_DEFINITIONS(RobotSystem);
 
-    hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo &info) override;
+    CallbackReturn on_init(const hardware_interface::HardwareInfo &info) override;
 
-    hardware_interface::CallbackReturn on_configure(
-        const rclcpp_lifecycle::State &previous_state) override;
+    // CallbackReturn on_configure(const rclcpp_lifecycle::State &previous_state) override;
 
     std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
 
     std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
 
-    hardware_interface::CallbackReturn on_activate(
-        const rclcpp_lifecycle::State &previous_state) override;
+    // CallbackReturn on_activate(const rclcpp_lifecycle::State &previous_state) override;
 
-    hardware_interface::CallbackReturn on_deactivate(
-        const rclcpp_lifecycle::State &previous_state) override;
+    // CallbackReturn on_deactivate(const rclcpp_lifecycle::State &previous_state) override;
 
     hardware_interface::return_type read(const rclcpp::Time &time, const rclcpp::Duration &period) override;
 
     hardware_interface::return_type write(const rclcpp::Time &time, const rclcpp::Duration &period) override;
+
+  protected:
+    std::vector<double> joint_position_command_;
+    // std::vector<double> joint_velocities_command_;
+    std::vector<double> joint_position_;
+    std::vector<double> joint_velocities_;
+
+    std::unordered_map<std::string, std::vector<std::string>> joint_interfaces = {
+        {"position", {}},
+        {"velocity", {}}};
   };
 
 } // namespace robot_hardware
--- robot_hardware/src/robot_system.cpp
+++ robot_hardware/src/robot_system.cpp
@@ -1,51 +1,108 @@
 #include "robot_hardware/robot_system.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+using return_type = hardware_interface::return_type;
 
 namespace robot_hardware
 {
 
-    hardware_interface::CallbackReturn RobotSystem::on_init(const hardware_interface::HardwareInfo &)
+    CallbackReturn RobotSystem::on_init(const hardware_interface::HardwareInfo &info)
     {
-        return hardware_interface::CallbackReturn::SUCCESS;
+        if (hardware_interface::SystemInterface::on_init(info) != CallbackReturn::SUCCESS)
+            return CallbackReturn::ERROR;
+
+        joint_position_.assign(info_.joints.size(), 0);
+        joint_velocities_.assign(info_.joints.size(), 0);
+        joint_position_command_.assign(info_.joints.size(), 0);
+        // joint_velocities_command_.assign(info_.joints.size(), 0);
+
+        for (const auto &joint : info_.joints)
+        {
+            for (const auto &interface : joint.state_interfaces)
+            {
+                joint_interfaces[interface.name].push_back(joint.name);
+            }
+        }
+
+        return CallbackReturn::SUCCESS;
     }
 
-    hardware_interface::CallbackReturn RobotSystem::on_configure(
-        const rclcpp_lifecycle::State &)
-    {
-        return hardware_interface::CallbackReturn::SUCCESS;
-    }
+    // CallbackReturn RobotSystem::on_configure(
+    //     const rclcpp_lifecycle::State &)
+    // {
+    //     return CallbackReturn::SUCCESS;
+    // }
 
     std::vector<hardware_interface::StateInterface> RobotSystem::export_state_interfaces()
     {
         std::vector<hardware_interface::StateInterface> state_interfaces;
+
+        int ind = 0;
+        for (const auto &joint_name : joint_interfaces["position"])
+        {
+            state_interfaces.emplace_back(joint_name, "position", &joint_position_[ind++]);
+        }
+
+        ind = 0;
+        for (const auto &joint_name : joint_interfaces["velocity"])
+        {
+            state_interfaces.emplace_back(joint_name, "velocity", &joint_velocities_[ind++]);
+        }
+
         return state_interfaces;
     }
 
     std::vector<hardware_interface::CommandInterface> RobotSystem::export_command_interfaces()
     {
         std::vector<hardware_interface::CommandInterface> command_interfaces;
+
+        int ind = 0;
+        for (const auto &joint_name : joint_interfaces["position"])
+        {
+            command_interfaces.emplace_back(joint_name, "position", &joint_position_command_[ind++]);
+        }
+
+        // ind = 0;
+        // for (const auto &joint_name : joint_interfaces["velocity"])
+        // {
+        //     command_interfaces.emplace_back(joint_name, "velocity", &joint_velocities_command_[ind++]);
+        // }
+
         return command_interfaces;
     }
 
-    hardware_interface::CallbackReturn RobotSystem::on_activate(
-        const rclcpp_lifecycle::State &)
+    // CallbackReturn RobotSystem::on_activate(
+    //     const rclcpp_lifecycle::State &)
+    // {
+    //     return CallbackReturn::SUCCESS;
+    // }
+
+    // CallbackReturn RobotSystem::on_deactivate(
+    //     const rclcpp_lifecycle::State &)
+    // {
+    //     return CallbackReturn::SUCCESS;
+    // }
+
+    return_type RobotSystem::read(const rclcpp::Time &, const rclcpp::Duration &)
     {
-        return hardware_interface::CallbackReturn::SUCCESS;
+        // for (size_t i = 0; i < joint_velocities_command_.size(); i++)
+        // {
+        //     joint_velocities_[i] = joint_velocities_command_[i];
+        //     joint_position_[i] += joint_velocities_command_[i] * period.seconds();
+        // }
+
+        for (size_t i = 0; i < joint_position_command_.size(); i++)
+        {
+            joint_position_[i] = joint_position_command_[i];
+        }
+
+        return return_type::OK;
     }
 
-    hardware_interface::CallbackReturn RobotSystem::on_deactivate(
-        const rclcpp_lifecycle::State &)
+    return_type RobotSystem::write(const rclcpp::Time &, const rclcpp::Duration &)
     {
-        return hardware_interface::CallbackReturn::SUCCESS;
-    }
-
-    hardware_interface::return_type RobotSystem::read(const rclcpp::Time &, const rclcpp::Duration &)
-    {
-        return hardware_interface::return_type::OK;
-    }
-
-    hardware_interface::return_type RobotSystem::write(const rclcpp::Time &, const rclcpp::Duration &)
-    {
-        return hardware_interface::return_type::OK;
+        return return_type::OK;
     }
 
 } // namespace robot_hardware
